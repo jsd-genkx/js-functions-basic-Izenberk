ceef05fbcfc41bf42a188c558baf093a65b7f11a
Izenberk <aceskorn555@outlook.com>
2025-07-16 09:33:01 +0700

    task02_solution

--- task02.js
+++ task02.js
@@ -14,9 +14,24 @@ Task 02: Check Even or Odd
 */
 
 // 👨‍💻 TODO-01: Write your code below to complete the task as described above.
+function checkEvenOrOdd(number) {
+    if (number % 2 === 0 && typeof number === 'number') {
+        return "Even";
+    } else if (number % 2 !== 0 && typeof number === 'number') {
+        return "Odd";
+    } else {
+        return "Invalid input";
+    }
+}
+
+// Use Ternary Operator with arrow function
+// const checkEvenOrOddArrow = (number) => {
+//     return number % 2 === 0 ? "Even" : "Odd";
+// }
 
 // Example Test Cases
 console.log(checkEvenOrOdd(4)); // "Even"
 console.log(checkEvenOrOdd(9)); // "Odd"
 console.log(checkEvenOrOdd(0)); // "Even"
 console.log(checkEvenOrOdd(-3)); // "Odd"
+// console.log(checkEvenOrOdd("1"));
